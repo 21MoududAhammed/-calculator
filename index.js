3b50fb06ea4c00963faf6f35703a9f5ab768e22b
21MoududAhammed <21moudud@gmail.com>
2022-08-26 23:23:04 +0600

    silly calculater done for practice bouble withoout error handler

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
+let sign = '';
 document.getElementById('all-buttons-action').addEventListener('click',function(event){
-    if(isNaN(event.target.innerText)){
-        console.log('not a number');
+    const inputField = document.getElementById('inputField');
+    const clickButton = event.target.innerText;
+    if(isNaN(clickButton)){
+        // select sign to take a action 
+        if(clickButton === '+'){
+            inputField.value += clickButton;
+            sign = '+';
+        }
+        else if(clickButton === '-'){
+            inputField.value += clickButton;
+            sign = '-';
+        }
+        else if(clickButton === '*'){
+            inputField.value += clickButton;
+            sign = '*';
+        }
+        else if(clickButton === '/'){
+            inputField.value += clickButton;
+            sign = '/';
+        }
+        else if(clickButton === 'AC'){
+            inputField.value = '';
+        }
+        else if(clickButton === "<"){
+            const data = inputField.value;
+            const result = data.split('');
+            result.pop();
+            const currentData = result.join('');
+            inputField.value = currentData;   
+        }
+        // calculation and show result on display 
+        else if(clickButton === '='){
+            const data = inputField.value
+            if(sign === '+'){
+                const numbers = data.split('+');
+                const result = numbers.reduce((sum,number) => sum += parseInt(number) ,0);
+                inputField.value += `=${result}`;
+            }
+            else if(sign === '-'){
+                const numbers = data.split('-');
+                const result = numbers[0]-numbers[1];
+                inputField.value += `=${result}`;
+            }
+            else if(sign === '*'){
+                const numbers = data.split('*');
+                const result = numbers.reduce((sum,number) => sum *= parseInt(number) ,1);
+                inputField.value += `=${result}`;
+            }
+            else if(sign === '/'){
+                const numbers = data.split('/');
+                const result = numbers[0]/numbers[1];
+                inputField.value += `=${result}`;
+            }   
+        }
     }else{
-        console.log('number');
+        //add nuber one by one and show it on display till than final calculation 
+        inputField.value += clickButton;
+
     }
 })
---
+++ test.js
@@ -0,0 +1,4 @@
+const data = ['1', '0', '0', '0', '0'];
+data.join(',')
+console.log(data);
+
